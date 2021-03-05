c817645c498cf883c45f50bbc9da691e4d77cc81
root <root@TUF-PEDRO.localdomain>
2021-03-05 15:10:47 -0600

    Preparando el terreno

---
+++ index.php
@@ -0,0 +1,28 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="src/css/materialize.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">MVC</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="">Modelo</a></li>
+        <li><a href="">Vista</a></li>
+        <li><a href="">Controlador</a></li>
+      </ul>
+    </div>
+  </nav>
+    <body>
+     <h1>Vamos a aprender a hacer un login increible<h1>
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="src/js/materialize.js"></script>
+    </body>
+  </html>
