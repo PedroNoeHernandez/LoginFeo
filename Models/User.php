a43171530633a196fd29bb6ce7f097136e5229ca
root <root@TUF-PEDRO.localdomain>
2021-03-05 19:22:39 -0600

    modelo

--- Models/User.php
+++ Models/User.php
@@ -1,29 +1,33 @@
-<?
-include '../Config/DataBase';
+<?php
+//include '../Config/DataBase.php';
 
 class User{
 
     public $correo ='';
-    private $contrasenia = '';
-    private $id = 0;
+    public $contrasenia = '';
+    public $id = 0;
+    public $host = 'localhost';
+    public $user = 'pedro';
+    public  $password = 'pedro';
+    public $database ='mvc';
 
-    function __construct($correo,$contasenia) {
+    function __construct($correo,$contrasenia) {
         $this->correo = $correo;
         $this->contrasenia = $contrasenia;
     }
 
     function validar(){
-        $sql = "SELECT TOP 1 * FROM Usuarios WHERE Correo = '$this->correo' AND contrasenia = '$this->contrasenia'";
-        $resultado = mysqli_query($conexion, $sql);
+	$conexion = mysqli_connect($this->host,$this->user,$this->password,$this->database);
+        $sql = "SELECT * FROM Usuarios WHERE Correo = '$this->correo' AND contrasenia = '$this->contrasenia'";
+        $result = mysqli_query($conexion, $sql);
         if(mysqli_num_rows($result) > 0){
-            while($row = mysql_fetch_assoc($result)) {
-                $this->id = $row[id];
-                return true;
-            }
-        }else{
-            return false;
-        }
-    }
+            while($row = mysqli_fetch_assoc($result)) {
+                $this->id = $row['id'];
+		//mysqli_close($conexion);
+	    }
+            return true;
+	}else{
+	return false;
+	}
 }
-
-?>
+}
