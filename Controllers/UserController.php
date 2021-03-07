447ef478e6bd7bcb2d77293993ef3adb14043f41
root <root@TUF-PEDRO.localdomain>
2021-03-07 17:12:14 -0600

    conectando la vista

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -3,8 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include '../Models/User.php';
-
-$Usuario = new User('ejemplo@ejemplo.com','ejemplo');
+$correo = $_GET['correo'];
+$contrasenia = $_GET['contrasenia'];
+$Usuario = new User($correo,$contrasenia);
 echo $Usuario->contrasenia; // "<br>".$Usuario->correo;
 if($Usuario->validar()){
     session_start();
